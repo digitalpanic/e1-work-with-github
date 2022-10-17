cc7686d5a80a4e9e3be282c2178d63e01e43c21c
digitalpanic <dieallwissendemuellhalde@gmail.com>
2022-10-17 09:49:53 +0200

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
  #!/bin/bash         
 
 echo "Hey, Automation Star!"
-echo "My Name is Matthias Kuehn and I work as an Test Automation Engineer. "
+echo "My Name is Matthias Kühn and I work as an Test Automation Engineer. "
 
 var1=${1:-not_set}
 
